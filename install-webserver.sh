3866fc5579e1f51c1d5790fb81ae22eb6cb94da8
ruoxijing <rjing@hawk.iit.edu>
2015-10-04 12:14:48 -0500

    Added code to pull new index.html from other github account

--- install-webserver.sh
+++ install-webserver.sh
@@ -3,4 +3,10 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2 git
 
+git clone https://github.com/ruoxijing/itmo-544-2015fall.git
+
+mv ./itmo-544-2015fall/images /var/www/html/images
+mv ./itmo-544-2015fall/index.html /var/www/html
+mv ./itmo-544-2015fall/page2.html /var/www/html
+
 echo "Hello!" > /tmp/hello.txt
