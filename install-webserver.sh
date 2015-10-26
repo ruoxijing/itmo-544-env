7bfc2f940670602b8ad36f4ea8041b768bfb747c
ruoxijing <rjing@hawk.iit.edu>
2015-10-26 15:44:08 -0500

    Edited install-webserver.sh for MP1

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl
 
 git clone https://github.com/ruoxijing/itmo-544-2015fall.git
 
 mv ./itmo-544-2015fall/images /var/www/html/images
 mv ./itmo-544-2015fall/index.html /var/www/html
 mv ./itmo-544-2015fall/page2.html /var/www/html
+mv ./itmo-544-2015fall/*.php /var/www/html
+
+curl -sS https://getcomposer.org/installer | php
+
+php composer.phar require aws/aws-sdk-php
+
+mv vendor /var/www/html
 
 echo "Hello!" > /tmp/hello.txt
